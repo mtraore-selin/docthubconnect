34baaa1d0044913779813c93020b4868f7bf8337
Mohamed <mohamed@MacBook-Pro.lan>
2024-01-18 23:34:07 +0100

    add build forlder for render deployment

---
+++ client/build/static/js/856.2a834a03.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkclient=self.webpackChunkclient||[]).push([[856],{6856:(t,a,l)=>{l.d(a,{lE7:()=>h});var e=l(9983);function h(t){return(0,e.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M17 7l-1.41 1.41L18.17 11H8v2h10.17l-2.58 2.58L17 17l5-5zM4 5h8V3H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h8v-2H4V5z"}}]})(t)}}}]);
+//# sourceMappingURL=856.2a834a03.chunk.js.map
